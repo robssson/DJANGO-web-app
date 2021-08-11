59566360e360dd1d0f0ba29d827c3c2ce0054bab
Tomek <tomekradecki@gmail.com>
2021-08-11 19:46:37 +0200

    Return tutorials as a body of homepage

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Tutorial
 
 
 def homepage(request):
-    return HttpResponse('Hello. It works!')
+    return render(request=request,
+                  template_name='main/home.html',
+                  context={'tutorials': Tutorial.objects.all()})
 
