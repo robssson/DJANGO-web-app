0146917a130d0ef0e953be56e33d32b792e80d53
Tomek <tomekradecki@gmail.com>
2021-08-16 08:31:56 +0200

    Incorporate new functionality (register mode)

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
+app_name = 'main'
+
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path("register/", views.register, name="register"),
 ]
